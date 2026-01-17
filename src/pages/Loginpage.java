9fed491d58a3f66231b244e034dad7dcc624a505
Daniel Diraviam J <danieldiraviamj@Mac.communityfibre.co.uk>
2026-01-17 20:37:57 +0000

    added/updated/deleted files

--- src/pages/Loginpage.java
+++ src/pages/Loginpage.java
@@ -1,4 +1,12 @@
 package pages;
 
 public class Loginpage {
+
+    public static void main(String a[])
+    {
+        System.out.println("Hello LP");
+        String username ="admin";
+        String pwd = "admin";
+        System.out.println("Logged in");
+    }
 }
